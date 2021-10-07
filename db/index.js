52dd3bd91d931613228bd27667aa989ed88775e6
Eric Flores <ericfullstack@gmail.com>
2021-10-06 19:56:01 -0700

    main

--- db/index.js
+++ db/index.js
@@ -1,6 +1,5 @@
-const { Client } = require("pg"); // imports the pg module
+const { Client } = require("pg"); 
 
-// supply the db name and location of the database
 const client = new Client("postgres://localhost:5432/juicebox-dev");
 
 async function getAllUsers() {
